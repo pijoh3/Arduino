3ab2bd1e6e8ed0a2be5b577a898f1cf1cf6c064e
pijoh3 <42169922+pijoh3@users.noreply.github.com>
2018-08-20 09:07:22 +0900

    선풍기 아두이노 프로젝트
    LCH_M : 마스터 초음파센서, LCD 연결. 블루투스로 초음파센서로 얻은 거리 전송
    PBJ_S : 슬레이브. 스위치, LED(인터럽트), 블루투스로 얻은 데이터를 바탕으로 선풍기 제어.
    스위치로 끄고 켤 수 있고, 거리가 가까워지면 꺼지면서 LED가 커지고, 거리가 멀어지면 꺼지도록 제어

---
+++ LCH_M.ino
@@ -0,0 +1,146 @@
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#define Trig 8
+#define Echo 9
+
+SoftwareSerial mySerial(2, 3);
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
+
+void setup ()
+{
+  Serial.begin(9600);
+  Serial.println("Hello World!");
+  mySerial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("    Distance");
+}
+
+void loop()
+{
+  long Duration = 0;
+  
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  Duration = pulseIn(Echo, HIGH);
+
+  Duration = Duration / 2.9 / 2 / 10;
+
+  Serial.print("Distance = ");
+  Serial.println(Duration);
+  delay(300);
+  mySerial.write(Duration);
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(7, 1);
+  lcd.print(Duration);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#define Trig 8
+#define Echo 9
+
+SoftwareSerial mySerial(2, 3);
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
+
+void setup ()
+{
+  Serial.begin(9600);
+  Serial.println("Hello World!");
+  mySerial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("    Distance");
+}
+
+void loop()
+{
+// {
+ 
+//    Serial.write(mySerial.read());
+// }
+
+//  if(Serial.available())
+//  {
+//    mySerial.write(Serial.read());
+//  }
+   
+  // lcd.print("    DISTANCE");
+
+  long Duration = 0;  
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  Duration = pulseIn(Echo, HIGH);
+
+  Duration = Duration / 2.9 / 2 / 10;
+
+  Serial.print("Distance = ");
+  Serial.println(Duration);
+  delay(300);
+  mySerial.write(Duration);
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(7, 1);
+  lcd.print(Duration);
+  //delay(500);
+
+}
---
+++ PBJ_S.ino
@@ -0,0 +1,63 @@
+#include <SoftwareSerial.h>
+
+#define buttonpin 7
+#define motor 8
+#define TX 9
+#define RX 10
+#define lpin 2
+
+SoftwareSerial mySerial(TX, RX);
+boolean buttonstate = 0;
+long data;
+
+void setup() {
+pinMode(buttonpin, INPUT_PULLUP);
+pinMode(motor, OUTPUT);
+pinMode(lpin, OUTPUT);
+Serial.begin(9600);
+mySerial.begin(9600);
+Serial.println("Hello");
+attachInterrupt(0, sstop, RISING);
+}
+
+void loop() {  
+delay(330);
+data = mySerial.read();
+Serial.println(data);
+
+if (digitalRead(buttonpin) == 0)
+{
+  if ((buttonstate == 0) && (data < 30))
+  {
+    digitalWrite(motor, HIGH);
+  }
+  else if (buttonstate == 1)
+  {
+    digitalWrite(motor, LOW);
+  }
+
+  buttonstate = !buttonstate;  
+}
+
+if ((data < 10) && (data >= 0))
+{
+  digitalWrite(lpin, HIGH);
+  delay(10);
+  digitalWrite(lpin, LOW);
+}
+
+if ((data > 10) && (data < 30) && (buttonstate == 1))
+{
+  digitalWrite(motor, HIGH);
+}
+
+if (data >= 30)
+{
+  digitalWrite(motor, LOW);
+  buttonstate = !buttonstate;
+}
+}
+
+void sstop() {
+digitalWrite(motor, LOW);
+}
